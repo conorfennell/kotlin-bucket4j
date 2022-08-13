6ebf0ce7710f57e26f3d2958634742b14b0635cc
conorfennell <fennell.conor@gmail.com>
2022-08-13 12:15:41 +0100

    add schedule example

--- src/main/kotlin/com/idiomcentric/App.kt
+++ src/main/kotlin/com/idiomcentric/App.kt
@@ -3,9 +3,13 @@ package com.idiomcentric
 import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.Bucket
 import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class BucketExamples {
     companion object {
+        val scheduleExecutor = Executors.newScheduledThreadPool(1)
         fun bandwidth() {
             val limit = Bandwidth.simple(1, Duration.ofSeconds(10L))
             val bucket = Bucket.builder().addLimit(limit).build()
@@ -14,9 +18,26 @@ class BucketExamples {
                 println(bucket.tryConsume(1L))
             }
         }
+
+        fun scheduleBandwidth() {
+            val limit = Bandwidth.simple(1, Duration.ofSeconds(5L))
+            val bucket = Bucket.builder().addLimit(limit).build()
+
+            val scheduler = bucket.asScheduler()
+
+            repeat(2) {
+                // Consume a token from the token bucket.
+                // If a token is not available this method will block until the refill adds one to the bucket.
+                val token = scheduler.consume(1L, scheduleExecutor)
+                token.get()
+                println(Instant.now())
+            }
+            scheduleExecutor.awaitTermination(1L, TimeUnit.SECONDS)
+        }
     }
 }
 
 fun main() {
     BucketExamples.bandwidth()
+    BucketExamples.scheduleBandwidth()
 }
