c07bd3766526ddcd0d473e5ac9ae34fb7fa4a2a2
Conor Fennell <cfennell@tenable.com>
2022-08-19 11:06:08 +0100

    add gready example

--- src/main/kotlin/com/idiomcentric/App.kt
+++ src/main/kotlin/com/idiomcentric/App.kt
@@ -2,11 +2,30 @@ package com.idiomcentric
 
 import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.Bucket
+import io.github.bucket4j.Refill
 import java.time.Duration
 import java.time.Instant
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 
+internal enum class PricingPlan {
+    LOW {
+        val limit: Bandwidth
+            get() = Bandwidth.classic(20, Refill.intervally(20, Duration.ofHours(1)))
+    },
+    MEDIUM {
+        val limit: Bandwidth
+            get() = Bandwidth.classic(40, Refill.intervally(40, Duration.ofHours(1)))
+    },
+    HIGH {
+        val limit: Bandwidth
+            get() = Bandwidth.classic(100, Refill.intervally(100, Duration.ofHours(1)))
+    },
+    SPIKE {
+        val limit: Bandwidth
+            get() = Bandwidth.classic(1000, Refill.intervally(100, Duration.ofHours(1)))
+    }
+}
 class BucketExamples {
     companion object {
         private val scheduleExecutor: ScheduledExecutorService = Executors.newScheduledThreadPool(1)
@@ -34,10 +53,38 @@ class BucketExamples {
             }
             scheduleExecutor.shutdown()
         }
+
+        fun refill() {
+            val refill = Refill.intervally(10, Duration.ofMinutes(1))
+            val limit = Bandwidth.classic(10, refill)
+            val bucket: Bucket = Bucket.builder()
+                .addLimit(limit)
+                .build()
+
+            repeat(10) {
+                assert(bucket.tryConsume(1))
+            }
+            assert(!bucket.tryConsume(1))
+        }
+
+        fun refillGreedy() {
+            val refill = Refill.greedy(10, Duration.ofMinutes(1))
+            val limit = Bandwidth.classic(10, refill)
+            val bucket: Bucket = Bucket.builder()
+                .addLimit(limit)
+                .build()
+
+            repeat(10) {
+                assert(bucket.tryConsume(1))
+            }
+            assert(!bucket.tryConsume(1))
+        }
     }
 }
 
 fun main() {
     BucketExamples.bandwidth()
     BucketExamples.scheduleBandwidth()
+    BucketExamples.refill()
+    BucketExamples.refillGreedy()
 }
